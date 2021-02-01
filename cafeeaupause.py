bd9ddddebe8b2fa1fc643c40e70a9547ea274b8b
Stonesth <pierre.thonon@gmail.com>
2021-02-01 14:46:34 +0100

    To have the possibility to enter accent and other carachters place the value u before the string

---
+++ cafeeaupause.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from Tools import tools_v000 as tools
+import os
+from os.path import dirname
+from MyHours import myhours as m
+
+
+# -18 for the name of this project cafeAdministration
+save_path = os.path.dirname(os.path.abspath("__file__"))
+propertiesFolder_path = save_path + "/"+ "Properties"
+
+# Example of used
+# user_text = tools.readProperty(propertiesFolder_path, 'cafeAdministration', 'user_text=')
+
+# Open Browser
+tools.openBrowserChrome()
+
+m.connectToMyHours()
+m.enterCredentials()
+m.startTrackWithDescription(u'', u'Café + eau + pause', u'Business operations (non project / service related tasks) (P0710) - Business operations (non project / service related tasks)')
+
+# Exit Chrome
+tools.driver.quit()
